c208af2f373966bc9a9d1ed407c25e7de137f090
auyu <73044953+omwfh@users.noreply.github.com>
2024-11-22 00:27:05 -0800

    Update t+g handler.lua

--- t+g handler.lua
+++ t+g handler.lua
@@ -1,11 +1,9 @@
 local UserInputService = game:GetService("UserInputService")
 
 local KB = _G.K
-local TB = Enum.KeyCode.Zero
 local MG = Enum.KeyCode.P
 
 local LTimes = 0
-local hasExecuted = false
 
 local function notif(Title, Text, Duration)
     local CoreGUI = game:GetService("StarterGui")
@@ -16,30 +14,17 @@ local function notif(Title, Text, Duration)
     })
 end
 
-local function resetExecutionState()
-    hasExecuted = false
-end
-
 local function onKeyPress(input, gameProcessed)
     if gameProcessed then
         return
     end
 
     if input.KeyCode == KB then
-        if hasExecuted then
-            notif("Warning", "Already executed!", 2)
-            return
-        end
-
-        hasExecuted = true
         LTimes = LTimes + 1
         loadstring(game:HttpGet("https://raw.githubusercontent.com/omwfh/svelte/refs/heads/main/t%2Bg.lua"))()
 
         task.wait(0.01)
         print("Count: " .. LTimes)
-    elseif input.KeyCode == TB then
-        print("terminate works")
-        task.delay(5, resetExecutionState)
     elseif input.KeyCode == MG then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/omwfh/svelte/refs/heads/main/p.lua"))()
     end
