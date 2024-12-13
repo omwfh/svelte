afc0eb2e9fc5628f06051610627d687ca8c81bbb
auyu <73044953+omwfh@users.noreply.github.com>
2024-12-12 23:50:04 -0800

    Update optimized v2 t+g.lua

--- optimized v2 t+g.lua
+++ optimized v2 t+g.lua
@@ -19,10 +19,10 @@ local configHighPing = {
 }
 
 local configLowPing = {
-    value1 = 0.2,
+    value1 = 0.195,
     value2 = 0.005,
     value3 = 0.01,
-    value4 = 0.185
+    value4 = 0.165
 }
 
 local currentConfig = nil
